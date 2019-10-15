3fbb82d5ef8d14c61519f2a5c6cfae1e3f4a72c6
Xnphu <immrp188@gmail.com>
2019-10-16 02:19:19 +0700

    edit value

--- public/index.js
+++ public/index.js
@@ -1,135 +1,13 @@
 $(document).ready(function () {
-    var data = [
-        {
-            "DT_RowId": "row_1",
-            "stt": "1",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_2",
-            "stt": "2",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_3",
-            "stt": "3",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_4",
-            "stt": "4",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_5",
-            "stt": "5",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_6",
-            "stt": "6",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_7",
-            "stt": "7",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_8",
-            "stt": "8",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_9",
-            "stt": "9",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_10",
-            "stt": "10",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_11",
-            "stt": "11",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_12",
-            "stt": "12",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        }, {
-            "DT_RowId": "row_13",
-            "stt": "13",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
-        },
-        {
-            "DT_RowId": "row_14",
-            "stt": "14",
-            "title": "\'No Control Air\'Cotton cardigan",
-            "newspaper": "4800.00",
-            "category": "1",
-            "publish_date": "2014-03-13 04:17:16",
-            "update_time": "17",
-            "quality": "1"
+    $.ajax({
+        url: "http://localhost:8080/data",
+        type: "GET",
+        success: function(data) {
+            console.log(data)
+            create_article_table(data);
         }
-    ]
-    create_article_table(data);
+    })
+    
 })
 
 function create_article_table(article_list) {
@@ -153,7 +31,6 @@ function create_article_table(article_list) {
         }]
     });
 
-    // Activate an inline edit on click of a table cell
     $('#article_table').on('click', 'tbody td:not(:first-child)', function (event) {
         editor.inline(this);
     });
@@ -175,8 +52,8 @@ function create_article_table(article_list) {
         "rowCallback": function (row, data, index) {
             topic = $('td:eq(1)', row).html();
             $('td:eq(1)', row).html('<a href="' + data.href + '" target="_blank">' + topic + '</a>');
-            sentimentality = $('td:eq(6)', row).html();
-            $('td:eq(6)', row).html('<select value="' + data.quality + '"> <option value="positive"> Tích cực </option> <option value="negative"> Tiêu cực </option> <option value="neutral"> Trung tính </option></select>');
+            // sentimentality = $('td:eq(6)', row).html();
+            // $('td:eq(6)', row).html('<select value="' + data.quality + '"> <option value="positive"> Tích cực </option> <option value="negative"> Tiêu cực </option> <option value="neutral"> Trung tính </option></select>');
         },
         select: {
             style:    'os',
--- server.js
+++ server.js
@@ -10,8 +10,146 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 });
 
+app.get('/data', (req, res) => {
+    var data = [
+        {
+            "DT_RowId": "row_1",
+            "stt": "1",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_2",
+            "stt": "2",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_3",
+            "stt": "3",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_4",
+            "stt": "4",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_5",
+            "stt": "5",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_6",
+            "stt": "6",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tiêu cực"
+        }, {
+            "DT_RowId": "row_7",
+            "stt": "7",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Trung tính"
+        }, {
+            "DT_RowId": "row_8",
+            "stt": "8",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Trung tính"
+        }, {
+            "DT_RowId": "row_9",
+            "stt": "9",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tiêu cực"
+        }, {
+            "DT_RowId": "row_10",
+            "stt": "10",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_11",
+            "stt": "11",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tích cực"
+        }, {
+            "DT_RowId": "row_12",
+            "stt": "12",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Tiêu cực"
+        }, {
+            "DT_RowId": "row_13",
+            "stt": "13",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Trung tính"
+        },
+        {
+            "DT_RowId": "row_14",
+            "stt": "14",
+            "title": "\'No Control Air\'Cotton cardigan",
+            "newspaper": "4800.00",
+            "category": "1",
+            "publish_date": "2014-03-13 04:17:16",
+            "update_time": "17",
+            "quality": "Trung tính"
+        }
+    ]
+    res.send(data);
+});
+
 app.put('/table', (req, res) => {
     var data = [
+        {
+            "DT_RowId": "row_3",
+            "quality": "3"
+        },
         {
             "DT_RowId": "row_1",
             "quality": "1"
@@ -23,15 +161,23 @@ app.put('/table', (req, res) => {
     ]
     if (req) {
         var id = '';
-        var quality_value = '';
+        var new_quality_value = '';
         for (var key in req.body.data) {
             id = key;
-            quality_value = req.body.data[key]['quality'];
+            new_quality_value = req.body.data[key]['quality'];
             break;
             //console.log(key, req.body.data[key]);
         }
         console.log(id);
-        console.log(quality_value);
+        console.log(new_quality_value);
+
+        for (var key2 in data) {
+            if (data[key2]['DT_RowId']==id) {
+                data[key2]['quality'] = new_quality_value;
+                break;
+            };
+        }
+        console.log(data)
         res.json({success: "ok"})
         //To do: goi api de update du lieu
         // console.log(req.body.data)
